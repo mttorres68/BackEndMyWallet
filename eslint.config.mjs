6e5426bf4c0a8265b23ae36e46eddd738ceafecb
Mateus Torres <mateusmmt00@gmail.com>
2024-10-19 10:40:29 -0300

    chore: prettierrc and eslint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,10 +6,8 @@ import tseslint from 'typescript-eslint'
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   {
-    languageOptions: { globals: globals.node },
-    parserOptions: {
-      project: './tsconfig.json'
-    }
+    files: ['**/*.ts'],
+    languageOptions: { globals: globals.node, sourceType: 'script' }
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
